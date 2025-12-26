f7095772dc3961332ac59479ed0991c54515a3dd
kiriti-desarkar <kiriti.desarkar.mails@gmail.com>
2025-12-26 18:53:26 +0530

    PostConstruct and PreDestroy Annotations

---
+++ example6/src/main/java/com/example/main/Example6.java
@@ -0,0 +1,18 @@
+package com.example.main;
+
+import com.example.beans.Vehicle;
+import com.example.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Example6 {
+
+    public static void main(String[] args) {
+
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        Vehicle vehicle = context.getBean(Vehicle.class);
+        System.out.println("Vehicle Name from spring context is: " + vehicle.getName());
+        vehicle.printHello();
+        context.close();
+
+    }
+}
