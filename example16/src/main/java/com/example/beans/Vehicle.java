d20af3aa9f6de1bf4338158d8c1624ca7ae699d8
kiriti-desarkar <kiriti.desarkar.mails@gmail.com>
2025-12-29 13:31:31 +0530

    Prototype Bean Scope inside Spring

---
+++ example16/src/main/java/com/example/beans/Vehicle.java
@@ -0,0 +1,37 @@
+package com.example.beans;
+
+import com.example.services.VehicleServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component(value = "vehicleBean")
+public class Vehicle {
+
+    private String name="Honda";
+    private final VehicleServices vehicleServices;
+
+    @Autowired
+    public Vehicle(VehicleServices vehicleServices){
+        this.vehicleServices = vehicleServices;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public VehicleServices getVehicleServices() {
+        return vehicleServices;
+    }
+
+    @Override
+    public String toString(){
+        return "Vehicle name is: " + name;
+    }
+
+}
